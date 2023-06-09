97b34c22d982766d05f140f8bb9e56355a84ddf7
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-06-09 00:59:06 -0300

    fix null entrada salida

--- src/controller/inventario.controler.js
+++ src/controller/inventario.controler.js
@@ -171,7 +171,7 @@ export const getInventariosSelectNombres = async (req, res) => {
 const getIdInvenario = async () => {
   const [rows] = await con.query("SELECT id FROM inventario");
   return rows;
-}
+};
 
 export const getInventarioStockActual = async (req, res) => {
   try {
@@ -181,18 +181,21 @@ export const getInventarioStockActual = async (req, res) => {
 
     for (let index = 0; index < resultado.length; index++) {
       const element = resultado[index];
-      console.log(element.id);
       const [rows] = await con.query(
         `SELECT nombre, descripcion, 
                 SUM(CASE WHEN idcategoria = 1 THEN stock ELSE 0 END ) as Salida,
                 SUM(CASE WHEN idcategoria = 2 THEN stock ELSE 0 END ) as Entrada
                 FROM mercaderia 
                 INNER JOIN inventario ON inventario.id = mercaderia.idinventario 
-                WHERE idinventario = ${element.id};`,
+                WHERE idinventario = ${element.id};`
       );
-      listaEnviar.push({...rows[0], idinventario: element.id});
+      if (rows[0].Entrada == null) rows[0].Entrada = 0;
+
+      if (rows[0].Salida == null) rows[0].Salida = 0;
+
+      listaEnviar.push({ ...rows[0], idinventario: element.id });
     }
-    
+
     res.json(listaEnviar);
   } catch (error) {
     return res.status(500).send({ message: "Something wrong" });
