1debef1ae60c7652b749f86145e5b2345f52a4f3
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-06-12 11:48:14 -0300

    mercaderia entrada salida

--- src/controller/excel.controler.js
+++ src/controller/excel.controler.js
@@ -3,6 +3,9 @@ import ExcelJs from "exceljs";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const dir = dirname(__filename);
+
 export const getExcelMercaderiaEntrada = async (req, res) => {
   try {
     const [rows] = await con.query(
@@ -24,9 +27,38 @@ export const getExcelMercaderiaEntrada = async (req, res) => {
 
     worksheet.addRows(rows);
 
-    await workbook.xlsx.writeFile("mercaderia.xlsx");
+    await workbook.xlsx.writeFile("./src/controller/mercaderiaEntrada.xlsx");
 
-    res.download();
+    res.sendFile(dir + "/mercaderiaEntrada.xlsx");
+  } catch (error) {
+    return res.status(500).json({ message: "something goes wrong" });
+  }
+};
+
+export const getExcelMercaderiaSalida = async (req, res) => {
+  try {
+    const [rows] = await con.query(
+      `SELECT mercaderia.id,fecha,stock,proveedor,nombre,descripcion,idinventario
+                FROM mercaderia 
+                INNER JOIN inventario on mercaderia.idinventario = inventario.id 
+                WHERE idcategoria = 1;`
+    );
+
+    const workbook = new ExcelJs.Workbook();
+    const worksheet = workbook.addWorksheet("salida");
+
+    worksheet.columns = [
+      { header: "FECHA", key: "fecha", width: 10 },
+      { header: "CODIGO PRODUCTO", key: "nombre", width: 10 },
+      { header: "DESCRIPCION", key: "descripcion", width: 10 },
+      { header: "CANTIDAD", key: "stock", width: 10 },
+    ];
+
+    worksheet.addRows(rows);
+
+    await workbook.xlsx.writeFile("./src/controller/mercaderiaSalida.xlsx");
+
+    res.sendFile(dir + "/mercaderiaSalida.xlsx");
   } catch (error) {
     return res.status(500).json({ message: "something goes wrong" });
   }
@@ -45,9 +77,6 @@ export const getExcelInventario = async (req, res) => {
     ];
 
     worksheet.addRows(rows);
-
-    const __filename = fileURLToPath(import.meta.url);
-    const dir = dirname(__filename);
 
     await workbook.xlsx.writeFile("./src/controller/inventario.xlsx");
 
--- src/routes/excel.routes.js
+++ src/routes/excel.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getExcelMercaderiaEntrada,getExcelInventario } from '../controller/excel.controler.js';
+import { getExcelMercaderiaEntrada,getExcelInventario,getExcelMercaderiaSalida } from '../controller/excel.controler.js';
 
 const router = Router();
 
 router.get('/excel/mercaderia/entrada' , getExcelMercaderiaEntrada);
+router.get('/excel/mercaderia/salida', getExcelMercaderiaSalida);
 
 router.get('/excel/inventario' , getExcelInventario);
 
