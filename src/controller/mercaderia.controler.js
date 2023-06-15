492ce6ec775842570c3614b0bc1fc028b43db95d
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-06-15 12:22:10 -0300

    date

--- src/controller/mercaderia.controler.js
+++ src/controller/mercaderia.controler.js
@@ -22,7 +22,21 @@ export const getEntradaMercaderias = async (req, res) => {
             INNER JOIN inventario on mercaderia.idinventario = inventario.id 
             WHERE idcategoria = 2;`
     );
-    res.json(rows);
+
+    const listaEnviar = [];
+
+    for (let i = 0; i < rows.length; i++) {
+      const element = new Date(rows[i].fecha);
+
+      const year = element.getFullYear();
+      const mounth = element.getMonth();
+      const day = element.getDate();
+
+      const format = `${day}-${mounth}-${year}`;
+      listaEnviar.push({ ...rows[i], fecha: format });
+    }
+
+    res.json(listaEnviar);
   } catch (error) {
     return res.status(500).json({ message: "something goes wrong" });
   }
@@ -36,7 +50,20 @@ export const getSalidaMercaderias = async (req, res) => {
             INNER JOIN inventario on mercaderia.idinventario = inventario.id 
             WHERE idcategoria = 1;`
     );
-    res.json(rows);
+    const listaEnviar = [];
+
+    for (let i = 0; i < rows.length; i++) {
+      const element = new Date(rows[i].fecha);
+
+      const year = element.getFullYear();
+      const mounth = element.getMonth();
+      const day = element.getDate();
+
+      const format = `${day}-${mounth}-${year}`;
+      listaEnviar.push({ ...rows[i], fecha: format });
+    }
+
+    res.json(listaEnviar);
   } catch (error) {
     return res.status(500).json({ message: "something goes wrong" });
   }
@@ -48,63 +75,92 @@ export const getEntradaMercaderiasWhereNombre = async (req, res) => {
       `SELECT mercaderia.id,fecha,stock,proveedor,nombre,descripcion,idinventario
             FROM mercaderia 
             INNER JOIN inventario ON mercaderia.idinventario = inventario.id 
-            WHERE nombre = ? and idcategoria = 2`,[req.params.nombre]
+            WHERE nombre = ? and idcategoria = 2`,
+      [req.params.nombre]
     );
 
     if (rows.length <= 0) return res.status(404).json({ message: "No existe" });
 
-    res.json(rows);
+    const listaEnviar = [];
+
+    for (let i = 0; i < rows.length; i++) {
+      const element = new Date(rows[i].fecha);
+
+      const year = element.getFullYear();
+      const mounth = element.getMonth();
+      const day = element.getDate();
+
+      const format = `${day}-${mounth}-${year}`;
+      listaEnviar.push({ ...rows[i], fecha: format });
+    }
+
+    res.json(listaEnviar);
   } catch (error) {
     return res.status(500).json({ message: "something goes wrong" });
   }
 };
 
 export const getSalidaMercaderiasWhereNombre = async (req, res) => {
-    try {
-        const [rows] = await con.query(
-          `SELECT mercaderia.id,fecha,stock,proveedor,nombre,descripcion,idinventario
+  try {
+    const [rows] = await con.query(
+      `SELECT mercaderia.id,fecha,stock,proveedor,nombre,descripcion,idinventario
                 FROM mercaderia 
                 INNER JOIN inventario ON mercaderia.idinventario = inventario.id 
-                WHERE nombre = ? and idcategoria = 1`,[req.params.nombre]
-        );
-    
-        if (rows.length <= 0) return res.status(404).json({ message: "No existe" });
-          
-        res.json(rows);
-      } catch (error) {
-        return res.status(500).json({ message: "something goes wrong" });;
-      }
-};
+                WHERE nombre = ? and idcategoria = 1`,
+      [req.params.nombre]
+    );
 
-export const createMercaderia = async (req,res) => {
-  try {
-    const { fecha,factura,stock,proveedor,idinventario,idcategoria } = req.body;
+    if (rows.length <= 0) return res.status(404).json({ message: "No existe" });
 
-    const fechaDate = new Date(fecha);
-    
-    const [rows] = await con.query(
-      'INSERT INTO mercaderia (`factura`, `fecha`, `stock`, `proveedor`, `idcategoria`, `idinventario`) VALUES (?,?,?,?,?,?);',
-      [factura,fechaDate,stock,proveedor,idcategoria,idinventario]);
-    
-      res.send({
-        id: rows.insertId,
-        fecha,
-        factura,
-        stock,
-        proveedor,
-        idcategoria,
-        idinventario
-      });
+    const listaEnviar = [];
 
+    for (let i = 0; i < rows.length; i++) {
+      const element = new Date(rows[i].fecha);
+
+      const year = element.getFullYear();
+      const mounth = element.getMonth();
+      const day = element.getDate();
+
+      const format = `${day}-${mounth}-${year}`;
+      listaEnviar.push({ ...rows[i], fecha: format });
+    }
+
+    res.json(listaEnviar);
   } catch (error) {
     return res.status(500).json({ message: "something goes wrong" });
   }
-}
+};
 
-
-export const updateMercaderia = async (req,res) => {
+export const createMercaderia = async (req, res) => {
   try {
-    const { fecha,factura,stock,proveedor,idinventario,idcategoria } = req.body;
+    const { fecha, factura, stock, proveedor, idinventario, idcategoria } =
+      req.body;
+
+    const fechaDate = new Date(fecha);
+
+    const [rows] = await con.query(
+      "INSERT INTO mercaderia (`factura`, `fecha`, `stock`, `proveedor`, `idcategoria`, `idinventario`) VALUES (?,?,?,?,?,?);",
+      [factura, fechaDate, stock, proveedor, idcategoria, idinventario]
+    );
+
+    res.send({
+      id: rows.insertId,
+      fecha,
+      factura,
+      stock,
+      proveedor,
+      idcategoria,
+      idinventario,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "something goes wrong" });
+  }
+};
+
+export const updateMercaderia = async (req, res) => {
+  try {
+    const { fecha, factura, stock, proveedor, idinventario, idcategoria } =
+      req.body;
     const id = req.params.id;
 
     const fechaDate = new Date(fecha);
@@ -118,29 +174,34 @@ export const updateMercaderia = async (req,res) => {
             idcategoria = IFNULL(?,idcategoria),
             idinventario = IFNULL(?,idinventario)
         WHERE id = ?;`,
-      [factura,fechaDate,stock,proveedor,idcategoria,idinventario,id]);
-      
-      if (result.affectedRows === 0)
+      [factura, fechaDate, stock, proveedor, idcategoria, idinventario, id]
+    );
+
+    if (result.affectedRows === 0)
       return res.status(404).json({ message: "mercaderia not found" });
 
-      const [rows] = await con.query("SELECT * FROM mercaderia WHERE id = ?", [id]);
-      res.json(rows[0]);
-
+    const [rows] = await con.query("SELECT * FROM mercaderia WHERE id = ?", [
+      id,
+    ]);
+    res.json(rows[0]);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "something goes wrong" });
   }
-}
+};
 
-export const deleteMercaderia = async (req,res) => {
+export const deleteMercaderia = async (req, res) => {
   try {
-    const [result] = await con.query('DELETE FROM mercaderia WHERE (`id` = ?);', [req.params.id]);
+    const [result] = await con.query(
+      "DELETE FROM mercaderia WHERE (`id` = ?);",
+      [req.params.id]
+    );
 
     if (result.affectedRows <= 0)
       return res.status(404).json({ message: "No se encontro" });
 
-      res.status(200).send({ message: "Eliminado Correctamente" });
+    res.status(200).send({ message: "Eliminado Correctamente" });
   } catch (error) {
     return res.status(500).json({ message: "something goes wrong" });
   }
-}
+};
