2c75f772a50e04791cb0ea0a85cf0896cd398e1c
ivan di gruttola <ivansandigruttola@gmail.com>
2023-10-19 14:07:27 -0300

    send token to createNewUser

--- src/controller/auth.controler.js
+++ src/controller/auth.controler.js
@@ -31,11 +31,19 @@ export const createNewUser = async (req, res) => {
           [nombre, apellido, hash, gmail]
         );
 
+        const userForToken = {
+          id: rows[0].id,
+          nombre: rows[0].nombre,
+          apellido: rows[0].apellido,
+          gmail: rows[0].gmail,
+          isAdmin: rows[0].isAdmin,
+        };
+    
+        const token = jwt.sign(userForToken, JWT_SECRET);
+    
         res.json({
-          id: rows.insertId,
-          nombre,
-          apellido,
-          gmail,
+          ...userForToken,
+          token,
         });
       });
     } else
