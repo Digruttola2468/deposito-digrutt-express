184ec6a146ed54c45ff412979b97019c834435c2
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-11-02 20:57:53 -0300

    -

--- src/controller/clientes.controler.js
+++ src/controller/clientes.controler.js
@@ -38,10 +38,8 @@ export const getOneCliente = async (req, res) => {
 export const createCliente = async (req, res) => {
   let lenghtCliente = await lenghtClientes();
 
-  console.log("LENGTH: ",lenghtCliente);
   if(lenghtCliente != -1) {
     const id = lenghtCliente + 1;
-    console.log("ID: ",id);
     try {
       const { codigo ,cliente, domicilio, idLocalidad, mail, cuit } = req.body;
       const [rows] = await con.query(
