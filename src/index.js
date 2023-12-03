418d77dc880c37b7f5eb7355df325a5e2504cb0f
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-12-02 21:27:27 -0300

    express handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
-import cloudinary from "cloudinary";
-import { CLOUDINARY_APIKEY, CLOUDINARY_APISECRET } from "./config.js";
-
 import { PORT } from "./config.js";
+import __dirname from './utils.js';
 
 //ROUTES PRINCIPAL
 import localidad from "./routes/localidad.routes.js";
@@ -14,7 +12,9 @@ import tipoProductoRoute from "./routes/tipoProducto.routes.js";
 import inventario from "./routes/inventario.routes.js";
 import excel from "./routes/excel.routes.js";
 import facturaNegro from "./routes/facturaNegro.routes.js";
-import cloudinaryRoute from './routes/cloudinary.routes.js';
+import cloudinaryRoute from "./routes/cloudinary.routes.js";
+import views from './routes/views.routes.js';
+
 
 //ROUTES PRODUCCION
 import clientes from "./routes/clientes.routes.js";
@@ -35,12 +35,18 @@ import ClientesManager from "./class/ClientesManager.js";
 import FacturaNegroManager from "./class/FacturaNegroManager.js";
 import RemitosManager from "./class/RemitosManager.js";
 import AuthManager from "./class/AuthManager.js";
+import handlebars from "express-handlebars";
 
 const app = express();
 
 //Habilitamos que la URL pueda acceder a este proyecto
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+// Configurar el motor de plantillas
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
 
 //Habilitamos la lectura en JSON
 app.use(express.json());
@@ -54,25 +60,27 @@ export const remitosManager = new RemitosManager();
 export const authManager = new AuthManager();
 
 //
-app.use('/api',indexRoute);
-app.use('/api',mercaderiaRoute);
-app.use('/api',coloresRoute);
-app.use('/api',tipoProductoRoute);
-app.use('/api',inventario);
-app.use('/api',excel);
-app.use('/api',cloudinaryRoute);
+app.use("/api", indexRoute);
+app.use("/api", mercaderiaRoute);
+app.use("/api", coloresRoute);
+app.use("/api", tipoProductoRoute);
+app.use("/api", inventario);
+app.use("/api", excel);
+app.use("/api", cloudinaryRoute);
 
-app.use('/api',clientes);
+app.use("/api", clientes);
 
-app.use('/api',grafica);
+app.use("/api", grafica);
 
-app.use('/api',user);
+app.use("/api", user);
 
-app.use('/api',remito);
+app.use("/api", remito);
 
-app.use('/api',localidad);
+app.use("/api", localidad);
 
-app.use('/api',facturaNegro);
+app.use("/api", facturaNegro);
+
+app.use(views);
 
 app.get("/", (req, res) => {
   res.send("Page Principal");
