c7439752909831d8fc6043840661a9bafb30124b
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2024-02-10 03:04:53 -0300

    -

--- src/class/MercaderiaManager.js
+++ src/class/MercaderiaManager.js
@@ -14,7 +14,7 @@ export default class MercaderiaManager {
   async getMercaderia() {
     try {
       const [rows] = await con.query(
-        `SELECT mercaderia.id,fecha,stock,nombre,descripcion,categoria,idinventario,articulo,idremito,idFacturaNegro,idCliente AS idcliente
+        `SELECT mercaderia.id,mercaderia.observacion,fecha,stock,nombre,descripcion,categoria,idinventario,articulo,idremito,idFacturaNegro,idCliente AS idcliente
               FROM mercaderia 
                   LEFT JOIN inventario on mercaderia.idinventario = inventario.id
                   LEFT JOIN categoria on mercaderia.idcategoria = categoria.id
