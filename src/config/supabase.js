47e54f3de610f98893658115aeb7e30b319321d1
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2024-03-20 17:28:23 -0300

    -

--- src/config/supabase.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_KEY } from '../config.js'
-
-export const db_supabase = createClient('https://bbpwzetypbcxsollgsla.supabase.co', SUPABASE_KEY)
