aeb136044d942482e4ae75a7eeddc2444ea20ef4
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2024-04-21 16:53:07 -0300

    user listo

--- src/DAO/mysql/users.mysql.js
+++ src/DAO/mysql/users.mysql.js
@@ -1,4 +1,5 @@
 import con from "../../config/db.js";
+import { createHash } from "../../utils.js";
 
 export default class UsersMySql {
   constructor() {
@@ -15,29 +16,90 @@ export default class UsersMySql {
     return await con.query(`SELECT * FROM users WHERE gmail = ?`, [gmail]);
   }
 
+  getByGmailSync(gmail) {
+    return this.listUsers.find((elem) => elem.gmail == gmail);
+  }
+  getSync() {
+    return this.listUsers
+  }
+
   async getOne(uid) {
     return await con.query(`SELECT * FROM users WHERE id = ?;`, [uid]);
   }
 
   async insert(data) {
-    return await con.query(
-      `INSERT INTO users (nombre, apellido, gmail, password) VALUES (?,?,?,?) `,
-      [data.nombre, data.apellido, data.gmail, data.password]
-    );
+    try {
+      const [result] = await con.query(
+        `INSERT INTO users (nombre, apellido, gmail, password) VALUES (?,?,?,?) `,
+        [data.nombre, data.apellido, data.gmail, data.password]
+      );
+
+      this.listUsers.push({
+        id: result.insertId,
+        nombre: data.nombre,
+        apellido: data.apellido,
+        gmail: data.gmail,
+        password: createHash(data.password),
+        role: "user",
+        isGmailValidate: 0,
+        isGoogleAuth: 0,
+      });
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 
   async insertGoogle(data) {
-    return await con.query(
-      `INSERT INTO users (nombre, apellido, gmail, isGmailValidate, isGoogleAuth) VALUES (?,?,?,?,?) `,
-      [data.nombre, data.apellido, data.gmail, 0, 1]
-    );
+    try {
+      const result = await con.query(
+        `INSERT INTO users (nombre, apellido, gmail, isGmailValidate, isGoogleAuth) VALUES (?,?,?,?,?) `,
+        [data.nombre, data.apellido, data.gmail, 0, 1]
+      );
+
+      this.listUsers.push({
+        id: result.insertId,
+        nombre: data.nombre,
+        apellido: data.apellido,
+        gmail: data.gmail,
+        role: "user",
+        isGmailValidate: 0,
+        isGoogleAuth: 1,
+      });
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 
-  async updateValidateGmail (gmail) {
-    return await con.query(`UPDATE users SET isGmailValidate = 1 WHERE gmail = ?;`, [gmail])
+  async updateValidateGmail(gmail) {
+    try {
+      const result = await con.query(
+        `UPDATE users SET isGmailValidate = 1 WHERE gmail = ?;`,
+        [gmail]
+      );
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 
   async delete(iid) {
-    return await con.query("DELETE FROM users WHERE (`id` = ?);", [iid]);
+    try {
+      const result = await con.query("DELETE FROM users WHERE (`id` = ?);", [
+        iid,
+      ]);
+
+      const deleteByIdUser = this.listUsers.filter((elem) => elem.id != iid);
+
+      this.listUsers = deleteByIdUser;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 }
